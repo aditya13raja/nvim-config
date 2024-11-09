4b5faac787842f50d6c9bc66080f26a6b83750a8
aditya13raja <aditya13raja@gmail.com>
2024-11-09 18:39:23 +0000

    Add plugins neo-tree, lualine and break plugins to their own configs

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set relativenumber")
-vim.cmd("set number")
---vim.cmd("set tabstop=4")
---vim.cmd("set softtabstop=4")
---vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -22,31 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{"olimorris/onedarkpro.nvim", priority = 1000},	
-        {
-                'nvim-telescope/telescope.nvim', tag = '0.1.8',
-                dependencies = { 'nvim-lua/plenary.nvim' }
-        },
-        {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
 local opts = {}
 
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-
--- setting telescope shortcut
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
-local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "lua", "javascript", "html", "go", "css", "typescript", "java" },
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-
-require("onedarkpro").setup()
-vim.cmd.colorscheme "onedark"
+require("vim-options")
+require("lazy").setup("plugins")
