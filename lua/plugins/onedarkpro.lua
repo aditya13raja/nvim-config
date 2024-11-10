d08b51c033958ac3a294cea48f1551505dd1ab98
aditya13raja <aditya13raja@gmail.com>
2024-11-10 10:15:01 +0000

    Add and config LSP for necessary languages

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -1,6 +1,8 @@
 return {
-        "olimorris/onedarkpro.nvim", priority = 1000,
-        config = function()
-                vim.cmd.colorscheme "onedark"
-        end
+    "olimorris/onedarkpro.nvim", priority = 1000,
+    config = function()
+        vim.cmd.colorscheme "onedark"
+--        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,17 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+-- vim.opt.colorcolumn = "80"
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
